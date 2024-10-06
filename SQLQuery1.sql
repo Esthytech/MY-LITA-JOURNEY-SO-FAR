355a4cccd49018776ce8d174bbf0bbed6e7348d5
Esther Alfred <queency4favour@gmail.com>
2024-10-06 14:24:38 -0400

    Add files via upload
    MY QUERY

---
+++ SQLQuery1.sql
@@ -0,0 +1,27 @@
+ 
+
+Create table employee (
+staffid varchar (10) not null,
+firstname varchar (255) not null,
+secondname varchar (255),
+gender varchar (10),
+date_of_birth date,
+hiredate datetime,
+primary key (staffid)
+)
+
+select * from employee
+
+insert into employee (staffid,firstname, secondname, gender, date_of_birth, hiredate)
+values ( 'AB401', 'ayan', 'olakun', 'female', '1992-08-22', '2018-02-09'),
+( 'AB212', 'okorie', 'mercy', 'female','1988-10-09', '2018-10-09'),
+( 'AB223', 'joshua', 'chukwuemeka', 'male','1980-10-09', '2022-02-09'),
+( 'AB234', 'sanni', 'ibrahim', 'male','1958-10-09', '2019-09-23'),
+( 'AB254', 'mercy', 'olanipekun', 'female','1982-10-09', '2020-02-09'),
+( 'AB249', 'johnson', 'mercy', 'female','1982-10-09', '2019-12-09'),
+( 'AB298', 'ayomide', 'halleluyah', 'female', '1982-10-09','2018-07-11'),
+( 'AB260', 'deborah', 'justin', 'female','1982-10-09', '2018-02-09'),
+( 'AB281', 'wale', 'olanipekun', 'male','1982-10-09', '2018-02-09')
+select * from employee 
+............to drop table .........
+drop table employee
